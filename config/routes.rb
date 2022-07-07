9ae5f6375e2e71116bbf07b295442a5ce9ccd5b5
rahulgupta <rahul.g@simformsolutions.com>
2022-07-07 11:59:14 +0000

    Rails internationalizaion [07/07/2022] add data in en.yml file for dynamic data modify index.html.erb file inside

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    member do 
+      get "rahul"
+    end
+    
+    collection do 
+      get "rahulgupta"
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
